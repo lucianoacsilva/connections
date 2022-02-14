9c80f0b5996c560d04c7986cf00de1974f4cbde3
Luciano Augusto Campagnoli da Silva <lucianos@cpqd.com.br>
2022-02-14 15:01:04 -0300

    Adjusts consumer config with delete methods

--- src/main/java/com/pingr/Connections/application/KafkaConsumerService.java
+++ src/main/java/com/pingr/Connections/application/KafkaConsumerService.java
@@ -2,6 +2,7 @@ package com.pingr.Connections.application;
 
 import com.pingr.Connections.core.Account;
 import com.pingr.Connections.core.events.AccountCreatedEvent;
+import com.pingr.Connections.core.events.AccountDeletedEvent;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,19 @@ public class KafkaConsumerService {
             topics = "${topics.acc_created}",
             groupId = "${spring.kafka.consumer.group-id}"
     )
+
     public void handleAccountCreation(AccountCreatedEvent event) {
+        Account account = event.extract();
+        System.out.println(account);
+    }
+
+    @KafkaListener(
+            containerFactory = "accountDeletedEventKafkaListenerContainerFactory",
+            topics = "${topics.acc_deleted}",
+            groupId = "${spring.kafka.consumer.group-id}"
+    )
+
+    public void handleAccountRemoval(AccountDeletedEvent event) {
         Account account = event.extract();
         System.out.println(account);
     }
